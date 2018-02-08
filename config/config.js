21f0e1e4aa06d23d56bb64552534e3f52e3bed4f
Hirwin Portuguez Salvatierra <hirwinportuguezsalvatierra@MacBook-Pro-de-Hirwin.local>
2018-02-08 16:00:22 -0500

    Seccion backend terminado

--- config/config.js
+++ config/config.js
@@ -1,1 +1,6 @@
 module.exports.SEED = '@este-es@-un-seed/dificil';
+
+
+// GOOGLE
+module.exports.GOOGLE_CLIENT_ID = '1060700550272-9bdnnjcufedsgjis423lfdk18p4jacg0.apps.googleusercontent.com';
+module.exports.GOOGLE_SECRET = '-MtIxG52gtDr6lGOSd7xuFiS';
